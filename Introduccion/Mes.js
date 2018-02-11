1617103c83fee584042e75523f8bf55572c0a774
Joseaam15 <35754250+Joseaam15@users.noreply.github.com>
2018-02-10 23:07:13 -0600

    Add files via upload

---
+++ Introduccion/Mes.js
@@ -0,0 +1,37 @@
+var Mes=10;
+
+   if(Mes ==12){
+   alert("es invierno");
+   }else if(Mes ==2){
+     alert("es invierno");
+   }else if(Mes ==1){
+     alert("es invierno");
+     //aqui termina invierno
+     
+     //aqui comienza Primavera
+   }if(Mes ==3){
+     alert("es primavera");
+   }else if(Mes==4){
+     alert("es primavera");
+   }else if(Mes ==5){
+     alert("es primavera");
+    
+     //aqui enpieza verano
+   }if(Mes ==6){
+     alert("es verano");
+   }else if(Mes ==7){
+     alert("es verano");
+   }if(Mes ==8){
+     alert("es verano");
+     
+     //aqui empieza otoño
+     
+   }if(Mes ==9){
+     alert("es otoño");
+   }else if(Mes ==10){
+     alert("es otoño");
+   }if(Mes ==11){
+     alert("es otoño");
+   }
+
+alert("Jose Andres Almendarez Mendez Carnet 18006306")
